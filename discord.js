e6acbc19412f984662616bb360bf3f771e75fa3d
maadcock <michael.l.adcock@gmail.com>
2019-10-29 20:01:07 -0400

    Added express server

--- discord.js
+++ discord.js
@@ -6,6 +6,9 @@ const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const v5Accept = "application/vnd.twitchtv.v5+json";
 const mixerClient = new Mixer.Client(new Mixer.DefaultRequestRunner());
 const MongoClient = require('mongodb').MongoClient;
+const express = require('express');
+const app = express();
+const port = 3000;
 
 // Auth
 const auth = require('./auth.json');
@@ -41,6 +44,9 @@ MongoClient.connect(mongoUrl, function(err, db) {
     const colUsers = dbo.collection("users");
     const colServers = dbo.collection("servers");
     const colTimeout = dbo.collection("timeout");
+
+    app.get('/', (req, res) => res.send("Hello world!"))
+    app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
 // Pull Server List from db
@@ -559,32 +565,32 @@ client.on('message', msg => {
                 newMsg = 'The prefix is currently set to `' + prefix + '`\n\n';
             }
             // I should clean this up at some point.
-            newMsg = newMsg + '**Profile Command List**\n';
-            newMsg = newMsg + '`' + prefix + 'createprofile` - Creates a profile on the bot\n';
-            newMsg = newMsg + '`' + prefix + 'deleteprofile` - Deletes your profile from the database\n';
-            newMsg = newMsg + '`' + prefix + 'profile` - Returns your profile\n';
-            newMsg = newMsg + '`' + prefix + 'whoami` - Returns your profile\n';
-            newMsg = newMsg + '`' + prefix + 'whois <user>` - Returns the users profile\n';
-            newMsg = newMsg + '`' + prefix + 'profile add <twitch|twitter|facebook|instagram>` - Adds Twitch|Twitter|Facebook|Instagram links to your profile\n';
-            newMsg = newMsg + '`' + prefix + 'profile del <twitch|twitter|facebook|instagram>` - Removes Twitch|Twitter|Facebook|Instagram links to your profile\n';
-            newMsg = newMsg + '\n';
-            newMsg = newMsg + '**Random Command List**\n';
-            newMsg = newMsg + '`' + prefix + 'catfact` - Returns a random fact about cats\n';
-            newMsg = newMsg + '`' + prefix + 'catfact <number>` - Returns the specificed cat fact\n';
-            newMsg = newMsg + '\n';
-            newMsg = newMsg + '**Twitch/Mixer Command List**\n';
-            newMsg = newMsg + '`' + prefix + 'live <username>` - Returns if Twitch user indicated is currently live\n';
-            newMsg = newMsg + '`' + prefix + 'stats <username>` - Returns Twitch stats of selected user\n';
-            newMsg = newMsg + '`' + prefix + 'mixerstats <username>` - Returns the Mixer stats of the selected user\n';
-            newMsg = newMsg + '\n';
-            newMsg = newMsg + '**Bot Commands**\n'
-            newMsg = newMsg + '`' + prefix + 'help` - Returns a list of bot commands\n';
-            newMsg = newMsg + '\n';
-            newMsg = newMsg + '**Admin Commands**\n';
-            newMsg = newMsg + '`' + prefix + 'prefix` - Changes the prefix for the bot in the server\n';
-            newMsg = newMsg + '`' + prefix + 'timeout` - Sends a user to designated timeout role\n';
-            newMsg = newMsg + '`' + prefix + 'timeout setrole <role>` - Designates a timeout role\n';
-            newMsg = newMsg + '`' + prefix + 'timeoutlist` - Lists currently timed out users\n';
+            newMsg += '**Profile Command List**\n';
+            newMsg += '`' + prefix + 'createprofile` - Creates a profile on the bot\n';
+            newMsg += '`' + prefix + 'deleteprofile` - Deletes your profile from the database\n';
+            newMsg += '`' + prefix + 'profile` - Returns your profile\n';
+            newMsg += '`' + prefix + 'whoami` - Returns your profile\n';
+            newMsg += '`' + prefix + 'whois <user>` - Returns the users profile\n';
+            newMsg += '`' + prefix + 'profile add <twitch|twitter|facebook|instagram>` - Adds Twitch|Twitter|Facebook|Instagram links to your profile\n';
+            newMsg += '`' + prefix + 'profile del <twitch|twitter|facebook|instagram>` - Removes Twitch|Twitter|Facebook|Instagram links to your profile\n';
+            newMsg += '\n';
+            newMsg += '**Random Command List**\n';
+            newMsg += '`' + prefix + 'catfact` - Returns a random fact about cats\n';
+            newMsg += '`' + prefix + 'catfact <number>` - Returns the specificed cat fact\n';
+            newMsg += '\n';
+            newMsg += '**Twitch/Mixer Command List**\n';
+            newMsg += '`' + prefix + 'live <username>` - Returns if Twitch user indicated is currently live\n';
+            newMsg += '`' + prefix + 'stats <username>` - Returns Twitch stats of selected user\n';
+            newMsg += '`' + prefix + 'mixerstats <username>` - Returns the Mixer stats of the selected user\n';
+            newMsg += '\n';
+            newMsg += '**Bot Commands**\n'
+            newMsg += '`' + prefix + 'help` - Returns a list of bot commands\n';
+            newMsg += '\n';
+            newMsg += '**Admin Commands**\n';
+            newMsg += '`' + prefix + 'prefix` - Changes the prefix for the bot in the server\n';
+            newMsg += '`' + prefix + 'timeout` - Sends a user to designated timeout role\n';
+            newMsg += '`' + prefix + 'timeout setrole <role>` - Designates a timeout role\n';
+            newMsg += '`' + prefix + 'timeoutlist` - Lists currently timed out users\n';
             msg.channel.send(newMsg);
         }
 
@@ -613,12 +619,12 @@ client.on('message', msg => {
             colUsers.find(user).toArray(function(err, result) {
                 if (result.length > 0) {
                     newMsg = "```";
-                    newMsg = newMsg + "Display Name: " + result[0].displayName + "\n";
-                    newMsg = newMsg + "User ID: " + result[0]._id + "\n";
-                    newMsg = result[0].twitch != undefined ? newMsg = newMsg + "Twitch: " + result[0].twitch + "\n" : newMsg;
-                    newMsg = result[0].twitter != undefined ? newMsg = newMsg + "Twitter: " + result[0].twitter + "\n" : newMsg;
-                    newMsg = result[0].facebook != undefined ? newMsg = newMsg + "Facebook: " + result[0].facebook + "\n" : newMsg;
-                    newMsg = newMsg + "```";
+                    newMsg += "Display Name: " + result[0].displayName + "\n";
+                    newMsg += "User ID: " + result[0]._id + "\n";
+                    newMsg = result[0].twitch != undefined ? newMsg += "Twitch: " + result[0].twitch + "\n" : newMsg;
+                    newMsg = result[0].twitter != undefined ? newMsg += "Twitter: " + result[0].twitter + "\n" : newMsg;
+                    newMsg = result[0].facebook != undefined ? newMsg += "Facebook: " + result[0].facebook + "\n" : newMsg;
+                    newMsg += "```";
                     msg.channel.send(newMsg);
                     //msg.channel.send("Profile '" + result[0].displayName + "' already exists with ID " + result[0]._id);
                 } else {
